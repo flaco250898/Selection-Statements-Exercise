6fec50a7ae42cffa0d267ae6114ba4eeaddbf52f
Liam Torres <liam.torreslopez@gmail.com>
2022-10-13 21:22:22 -0700

    created guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+       static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Welcome to the number guessing game");
+            Console.WriteLine("To continue please enter an upper limit number for the app.");
+
+            var userResponse = Console.ReadLine();
+            var upperLimit = int.Parse(userResponse);
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Now enter your guess and see below what you win.");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Your guess is to high.");
+            }
+            if (guess < number)
+            {
+                Console.WriteLine("Your guess is to low!");
+
+            }
+             
+            else 
+            {
+                Console.WriteLine("You're Correct");
+            }
         }
     }
 }
